c68d81ea92758469e938154f78715ff1a8f73d48
EC2 Default User <ec2-user@ip-172-31-26-194.us-east-2.compute.internal>
2021-02-04 06:18:30 +0000

    HW1 Final

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,73 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  ans=arr.inject(0, :+)
+  return ans
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  ans=arr.sort.last(2).sum
+  return ans
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return(!!arr.uniq.combination(2).detect { |x, y| x + y == n })
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  ans= "Hello, "+ name
+  return(ans)
 end
 
-def starts_with_consonant? s
-  # YOUR CODE HERE
+def starts_with_consonant?(s)
+  if /^[^aeiou\W]/i.match(s) == nil
+    return false
+  else
+    return true
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return true if s == "0"
+	/^[10]*00$/.match(s) != nil
 end
 
 # Part 3
-
 class BookInStock
 # YOUR CODE HERE
+  def initialize(isbn,price)
+      if isbn.length > 0 && price > 0
+        @isbn = isbn
+        @price = price
+      else
+        raise ArgumentError
+      end
+  end
+  
+  #getter for ISBN
+  def isbn
+    return @isbn
+  end
+  
+  #setter for ISBN
+  def isbn=(nisbn)
+    @isbn = nisbn
+  end
+  
+  #getter for price
+  def price
+    @price
+  end
+  
+  #setter for price
+  def price=(new_price)
+    @price = new_price
+  end
+  
+  def price_as_string
+    "$"+ '%.2f' % @price.round(2).to_s
+  end
+
 end
